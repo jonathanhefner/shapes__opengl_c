d48853a176a94251875b09881cc70585ddae1f81
Jonathan Hefner <jonathan.hefner@gmail.com>
2011-11-14 00:25:08 -0600

    Add vec3 data type and helper macros

--- src/shape.h
+++ src/shape.h
@@ -4,8 +4,29 @@
 #include <GL/gl.h>
 
 
+typedef GLfloat vec3[3];
+
+#define vx(V) ((V)[0])
+#define vy(V) ((V)[1])
+#define vz(V) ((V)[2])
+
+#define vec3_op_vec3(V1, OP, V2)  \
+  do {                            \
+    vx(V1) OP vx(V2);             \
+    vy(V1) OP vy(V2);             \
+    vz(V1) OP vz(V2);             \
+  } while (0);
+
+#define vec3_op_3f(V, OP, F1, F2, F3) \
+  do {                                \
+    vx(V) OP (F1);                    \
+    vy(V) OP (F2);                    \
+    vz(V) OP (F3);                    \
+  } while (0);
+
+
 typedef struct _Vertex {
-  GLfloat position[3];
+  vec3 position;
 } Vertex;
 
 typedef struct _Surface {
